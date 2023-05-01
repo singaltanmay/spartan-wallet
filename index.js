50dc0b17e909967f0ffe2e9e9b5d60e8a6004c2d
angelayang2018 <45446731+angelayang2018@users.noreply.github.com>
2023-05-01 15:01:51 -0700

    Changes

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 "use strict";
 
-const Mnemonic = require('./mnemonic.js').Mnemonic;
+const Mnemonic = require("./mnemonic.js").Mnemonic;
 
 let m = new Mnemonic();
 
 let seed = m.generateSeed();
+
+m.printMnemonic();
+
 console.log("The seed is " + seed);
 
 let masterKey = m.generateMasterKey(seed);
-console.log(masterKey);
-
 let keys = m.generateKeys(masterKey);
 console.log("privateKey: " + keys[0]);
 console.log("chainCode: " + keys[1]);
-console.log("publicKey: " + keys[2]);
+//console.log("publicKey: " + keys[2]);
 
