8fb6311d8a887108537eca5f368ee1f72bc2b730
Tanmay Singal <tanmaysingal@icloud.com>
2023-05-01 16:23:58 -0700

    changes

--- index.js
+++ index.js
@@ -16,7 +16,21 @@ console.log("privateKey: " + privateKey);
 console.log("chainCode: " + publicKey);
 //console.log("publicKey: " + keys[2]);
 
-const masterNode = m.generateMasterNode(masterKey);
+const {masterNode, masterExtendedPublicKey, masterChainCode} = m.generateMasterNode(masterKey);
 
-console.log("masterExtendedPublicKey: " + masterNode.masterExtendedPublicKey)
-console.log("masterChainCode: " + masterNode.masterChainCode)
+console.log("masterExtendedPublicKey: " + masterExtendedPublicKey)
+console.log("masterChainCode: " + masterChainCode)
+
+// Derive a child key from the master node
+const childNode = masterNode.derivePath("m/0");
+
+console.log(childNode.publicKey);
+
+// Get the child node's extended public key and chain code
+const childExtendedPublicKey = childNode.neutered().toBase58();
+const childChainCode = childNode.chainCode.toString('hex');
+
+console.log(childNode)
+
+console.log(childExtendedPublicKey);
+console.log(childChainCode);
